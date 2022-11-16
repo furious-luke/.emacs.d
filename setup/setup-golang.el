c37481989011c5c1ef31d3b4e154e42406b9b9a1
Luke Hodkinson <furious.luke@gmail.com>
2022-11-16 17:17:37 +1100

    :bug: Fix broken Golang settings

--- setup/setup-golang.el
+++ setup/setup-golang.el
@@ -5,4 +5,13 @@
                       (setq-default indent-tabs-mode 1)
                       (setq-default tab-width 2))))
 
+(defun project-find-go-module (dir)
+  (when-let ((root (locate-dominating-file dir "go.mod")))
+    (cons 'go-module root)))
+
+(cl-defmethod project-root ((project (head go-module)))
+  (cdr project))
+
+(add-hook 'project-find-functions #'project-find-go-module)
+
 (provide 'setup-golang)
