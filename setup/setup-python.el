e226fab749f20fb0fc2ebec8e83378d99903ce54
Luke Hodkinson <furious.luke@gmail.com>
2023-04-01 11:40:50 +1100

    :bug: Fix broken eglot and projects

--- setup/setup-python.el
+++ setup/setup-python.el
@@ -2,6 +2,11 @@
 ;; Python environment integrations.
 ;;
 
+(use-package python
+  :config
+  ;; Remove guess indent python message
+  (setq python-indent-guess-indent-offset-verbose nil))
+
 (use-package pyenv-mode
   :ensure t)
 
@@ -10,8 +15,13 @@
   :init
   (setenv "WORKON_HOME" "~/.pyenv/versions"))
 
-;; (use-package poetry
-;;   :ensure t
-;;   :config (add-hook 'prog-mode-hook #'poetry-tracking-mode))
+(use-package poetry
+  :ensure t
+  :defer t
+  :custom
+  (blacken-allow-py36 t)
+  (blacken-skip-string-normalization t)
+  :hook (python-mode-hook . blacken-mode))
+;; :config (add-hook 'prog-mode-hook #'poetry-tracking-mode))
 
 (provide 'setup-python)
